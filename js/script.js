4a13150094b7c02c9a925d07deb8a1106a697cc6
baranysf <baranrise@gmail.com>
2023-06-01 04:09:26 +0900

    proje tamamlandı

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function up_now(){
+    event.preventDefault();
+    var count;
+
+    count= document.getElementById('count').value;
+    count = parseInt(count) + 1;
+    document.getElementById('count').value=count;
+
+    console.log(count);
+
+}
+
+function down_now(){
+    event.preventDefault();
+    var count;
+    
+    count= document.getElementById('count').value;
+    count = parseInt(count) - 1;
+    document.getElementById('count').value=count;
+
+    console.log(count);
+}
