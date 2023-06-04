f68bf91f413d8e2494150bc9b5ae12e820eae9b4
baranysf <baranrise@gmail.com>
2023-06-05 04:05:23 +0900

    proje tamamlandı

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 function up_now(){
     event.preventDefault();
     var count;
+    document.getElementById('down').disabled=false;
 
     count= document.getElementById('count').value;
-    count = parseInt(count) + 1;
+    
+    if(count<10){
+        count = parseInt(count) + 1;
+        document.getElementById('down').disabled=false;
+    }
+    else{
+        alert('Sayaç 10 sayısından büyük olamaz.')
+    }
+
+    if(count==10){
+        document.getElementById('up').disabled=true;
+    }
     document.getElementById('count').value=count;
 
     console.log(count);
@@ -13,9 +25,24 @@ function up_now(){
 function down_now(){
     event.preventDefault();
     var count;
-    
+    document.getElementById('up').disabled=false;
+
     count= document.getElementById('count').value;
-    count = parseInt(count) - 1;
+
+    if(count>0){
+          count = parseInt(count) - 1;  
+    }
+    else(
+        alert('Sayaç 0 sayısından küçük olamaz.')
+    )
+    if(count== -10){
+        document.getElementById('down').disabled=true;
+    }
+    if(count< -10){
+        document.getElementById('down').disabled=false;
+    }
+
+
     document.getElementById('count').value=count;
 
     console.log(count);
